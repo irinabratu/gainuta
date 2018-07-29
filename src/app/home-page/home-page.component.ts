456fe634fe3ef597262b6519a2834149dfc5136e
irinabratu <iirinabratu@gmail.com>
2018-07-29 21:18:51 +0300

    extra hp prettify

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -9,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent implements OnInit {
 
     imageUrlArray = [
-        { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56748793/dbohn_170625_1801_0018.0.0.jpg', caption: 'The first slide', href: '#config' },
-        { url: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/9278671/jbareham_170917_2000_0124.jpg', clickAction: () => alert('custom click function') },
-        { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56789263/akrales_170919_1976_0104.0.jpg', caption: 'Apple TV', href: 'https://www.apple.com/' },
+        { url: 'assets/images/slide2.jpg', caption: 'Studio ae', href: 'http://studioae.ro' },
+        { url: 'assets/images/slide3.jpg', caption: 'Studio ae', href: 'http://studioae.ro' },
+        { url: 'assets/images/slide5.jpg', caption: 'Studio ae', href: 'http://studioae.ro' },
     ];
     
     constructor() { 
