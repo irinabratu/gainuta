1c913241a58512cfcdbd39c5f8b9c88c197be081
mihai-tincu <mihai.tincu.10@gmail.com>
2018-08-17 12:29:13 +0300

    place orders

--- src/app/checkout-page/checkout-page.component.ts
+++ src/app/checkout-page/checkout-page.component.ts
@@ -4,6 +4,7 @@ import { MessageService } from '../services/message.service';
 import { HttpClient } from '@angular/common/http';
 import { baseUrl } from '../../utils/constants';
 import { forEach } from '@angular/router/src/utils/collection';
+import { AlertMessageService } from '../shared/alert-message/alert-message.service';
 
 @Component({
   selector: 'checkout-page',
@@ -19,7 +20,8 @@ export class CheckoutPageComponent implements OnInit {
   showSpinner: boolean;
   totalAmount: number;
 
-  constructor(private messageService: MessageService, private http: HttpClient) {
+  constructor(private messageService: MessageService, private http: HttpClient,
+    private alertMessageService: AlertMessageService) {
 
     this.userId = 1;
     this.items = [];
@@ -65,5 +67,20 @@ export class CheckoutPageComponent implements OnInit {
 
   placeOrder() {
 
+    this.showSpinner = true;
+
+    this.http.post(baseUrl + 'ShoppingCart/PlaceOrder', this.items)
+      .subscribe(data => {
+
+        this.showSpinner = false;
+        this.alertMessageService.success('Order was placed successfully.');
+
+        this.messageService.sendCartRefresh(true);
+        this.fetchShoppingCartItems();
+      }, error => {
+
+        this.showSpinner = false;
+        this.alertMessageService.error(error.message);
+      });
   }
 }
