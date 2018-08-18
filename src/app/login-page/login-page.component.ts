2cfba3171a3c8bbbfdf6d9e6447cf3a6c7cf1e59
mihai-tincu <mihai.tincu.10@gmail.com>
2018-08-18 12:56:30 +0300

    login action

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { baseUrl } from '../../utils/constants';
+import { LoginEntity } from '../../model/LoginEntity';
+import { AlertMessageService } from '../shared/alert-message/alert-message.service';
+
 
 @Component({
   selector: 'login-page',
@@ -7,8 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  model = {};
-  constructor() { }
+  showSpinner: boolean;
+  model: LoginEntity = new LoginEntity();
 
-  ngOnInit() {  }
+  constructor(private http: HttpClient, private router: Router,
+    private alertMessageService: AlertMessageService) { }
+
+  ngOnInit() { }
+
+  onClickLogin() {
+    console.log(this.model);
+    if (!this.model.Email || !this.model.Password)
+      return;
+
+    this.showSpinner = true;
+
+    this.http.post(baseUrl + 'Users/Login', this.model).subscribe(data => {
+      if (data != null) {
+
+        localStorage.setItem('userToken', data.toString());
+        this.showSpinner = false;
+        this.router.navigateByUrl('/admin');
+      }
+      else {
+
+        this.showSpinner = false;
+        this.alertMessageService.error('Wrong credentials.');
+        const element = document.querySelector('#preFooter');
+        element.scrollIntoView();
+      }
+    }, error => {
+      this.showSpinner = false;
+      this.alertMessageService.error(error.message);
+      });
+  }
 }
---
+++ src/model/LoginEntity.ts
@@ -0,0 +1,4 @@
+export class LoginEntity {
+  Email: string;
+  Password: string;
+}
