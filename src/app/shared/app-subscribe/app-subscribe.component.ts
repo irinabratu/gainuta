493929a11679445129ba0a048bb635e0aa114314
mihai-tincu <mihai.tincu.10@gmail.com>
2018-08-20 10:09:00 +0300

    subscribers

--- src/app/shared/app-subscribe/app-subscribe.component.ts
+++ src/app/shared/app-subscribe/app-subscribe.component.ts
@@ -1,47 +1,62 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ValidationHelpers } from '../../../utils/ValidationHelpers';
 import { FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { AlertMessageService } from '../../shared/alert-message/alert-message.service';
+import { baseUrl } from '../../../utils/constants';
 
 @Component({
-    selector: 'app-subscribe',
-    templateUrl: './app-subscribe.component.html',
-    styleUrls: ['./app-subscribe.component.css'],
-    encapsulation: ViewEncapsulation.None
+  selector: 'app-subscribe',
+  templateUrl: './app-subscribe.component.html',
+  styleUrls: ['./app-subscribe.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class AppSubscribeComponent implements OnInit {
 
-    model: any;
+  model: any;
+  showSpinner: boolean;
 
-    constructor() {
+  constructor(private http: HttpClient,
+    private alertMessageService: AlertMessageService) {
+
+    this.model = {};
+  }
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  ngOnInit() { }
+
+  onClickSend() {
+
+    if (!this.model.Email) {
+      return;
+    }
+
+    let isValidEmail = ValidationHelpers.validateEmail(this.model.Email);
+    if (!isValidEmail) {
+      return;
+    }
+
+    this.showSpinner = true;
+    const element = document.querySelector('#preFooter');
+
+    this.http.post(baseUrl + 'Users/Subscribe', this.model)
+      .subscribe(data => {
+
+        this.showSpinner = false;
         this.model = {};
-    }
 
-    email = new FormControl('', [Validators.required, Validators.email]);
-    
-    ngOnInit() { }
+        this.alertMessageService.success('Subscription was saved successfully.');
+        element.scrollIntoView();
+      }, error => {
 
-     onClickSubscribe() {
+        this.alertMessageService.error(error.message);
+        element.scrollIntoView();
+      });
+  }
 
-         if (!this.model.Email) {
-             console.log('Email is required', 'Error!');
-             return;
-         }
-
-         let isValidEmail = ValidationHelpers.validateEmail(this.model.Email);
-         if (!isValidEmail) {
-           console.log('Invalid email', 'Error!');
-             return;
-         }
-
-          console.log('It\'s not working yet, sorry :)');
-        
-     }
-
-    onClickSend() {
-    }
-
-    getErrorMessage(){
-        return "invalid email";
-    }
+  getErrorMessage() {
+    return "invalid email";
+  }
 }
