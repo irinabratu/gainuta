5fd3029855b5ef6e26eabfceaddabec8137e7ddc
mihai-tincu <mihai.tincu.10@gmail.com>
2018-08-23 10:47:23 +0300

    reset password

--- src/app/forgot-password-page/forgot-password-page.component.ts
+++ src/app/forgot-password-page/forgot-password-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { baseUrl } from '../../utils/constants';
+import { LoginEntity } from '../../model/LoginEntity';
+import { AlertMessageService } from '../shared/alert-message/alert-message.service';
 
 @Component({
   selector: 'forgot-password-page',
@@ -7,7 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordPageComponent implements OnInit {
 
-  constructor() { }
+  showSpinner: boolean;
+  showForm: boolean = true;
+  email: string;
+
+  constructor(private http: HttpClient, private router: Router,
+    private alertMessageService: AlertMessageService) { }
 
   ngOnInit() { }
+
+  onClickReset() {
+    if (!this.email) {
+      return;
+    }
+
+    this.showSpinner = true;
+
+    this.http.post(baseUrl + 'Users/Reset', this.email)
+      .subscribe(data => {
+
+        this.showSpinner = false;
+
+        if (data != null) {
+          this.showForm = false;
+        }
+        else {
+          this.alertMessageService.error("Email is not in our system");
+          const element = document.querySelector('#preFooter');
+          element.scrollIntoView();
+        }      
+      }, error => {
+
+        this.showSpinner = false;
+        this.alertMessageService.error(error.message);
+        const element = document.querySelector('#preFooter');
+        element.scrollIntoView();
+      });
+  }
 }
