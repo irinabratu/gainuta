551468990f8e7d8448ccd295e0e1ee4ba90e0803
mihai-tincu <mihai.tincu.10@gmail.com>
2018-08-25 11:45:13 +0300

    set event active/inactiv

--- src/app/edit-event/edit-event.component.ts
+++ src/app/edit-event/edit-event.component.ts
@@ -34,8 +34,27 @@ export class EditEventComponent implements OnInit {
         this.events = data as EventEntity[];
         this.showSpinner = false;
       }, error => {
+        
+        this.showSpinner = false;
+        this.alertMessageService.error(error.message);
+      });
+  }
 
-        console.log(error);
+  onClickIsActive(event) {
+
+    this.showSpinner = true;
+
+    var model = new EventEntity();
+    model.Id = event.Id;
+    model.IsActive = !event.IsActive;
+
+    this.http.put(baseUrl + 'Events/SetActive/' + event.Id, model)
+      .subscribe(data => {
+        
+        this.showSpinner = false;
+        this.fetchEvents();
+      }, error => {
+        
         this.showSpinner = false;
         this.alertMessageService.error(error.message);
       });
