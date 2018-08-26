ba7dd34b3bdbe50de35121ad98fd6f15e315ec97
mihai-tincu <mihai.tincu.10@gmail.com>
2018-08-26 19:13:35 +0300

    portfolio panels

--- src/app/portfolio-page/portfolio-page.component.ts
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { baseUrl } from '../../utils/constants';
+import { PortfolioEntity } from '../../model/PortfolioEntity';
+import { MatExpansionPanel } from '@angular/material';
 
 @Component({
   selector: 'portfolio-page',
@@ -9,9 +13,58 @@ import { Component, OnInit } from '@angular/core';
 export class PortfolioPageComponent implements OnInit {
 
   showSpinner: boolean;
-
-  constructor() { }
+  archPanel: PortfolioEntity = new PortfolioEntity();
+  illustPanel: PortfolioEntity = new PortfolioEntity();
+  designPanel: PortfolioEntity = new PortfolioEntity();
+  
+  constructor(public http: HttpClient) {
+  }
 
   ngOnInit() {
+
+    this.fetchPortfolio();
+  }
+
+  fetchPortfolio() {
+
+    this.showSpinner = true;
+
+    this.http.get(baseUrl + 'Portfolio/GetActive')
+      .subscribe(data => {
+        
+        let archPanel;
+        let illustPanel;
+        let designPanel;
+        
+        var items = data as PortfolioEntity[];
+
+        items.forEach(function (item) {
+          if (item.Category == 'Achitecture') {
+            archPanel = item;
+          }
+          else if (item.Category == 'Illustration') {
+            illustPanel = item;
+          }
+          else if (item.Category == 'Design') {
+            designPanel = item;
+          }
+        });
+
+        this.archPanel = archPanel;
+        this.illustPanel = illustPanel;
+        this.designPanel = designPanel;
+
+        this.showSpinner = false;
+
+      }, fail => {
+
+        console.log(fail.message);
+
+      });
+  }
+
+  onClickPanel(panel: MatExpansionPanel) {
+    
+    console.log(panel);
   }
 }
---
+++ src/model/PortfolioEntity.ts
@@ -0,0 +1,8 @@
+export class PortfolioEntity {
+  Id: number;
+  Title: string;
+  Description: string;
+  Category: string;
+  Content: string;
+  IsActive: boolean;
+}
